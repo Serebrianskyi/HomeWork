9648f2733b039f80413673a891259b0cd4acb3d9
Roman Serebrianskyi <roma.serebryanskyi@gmail.com>
2019-04-16 13:54:32 +0300

    Singltone with refleclection

---
+++ src/main/java/com.homeWork/SingletonWithReflection.java
@@ -0,0 +1,33 @@
+package com.homeWork;
+import java.lang.reflect.Constructor;
+
+class Singleton {
+
+    private static final Singleton instance = new Singleton();
+
+    private Singleton() {}
+
+    public static Singleton getInstance() {
+        return instance;
+    }
+}
+
+public class SingletonWithReflection {
+    public static void main(String[] args) {
+        Singleton singletonInstance = Singleton.getInstance();
+        Singleton reflectionInstance = null;
+
+        try {
+            Constructor[] constructors = Singleton.class.getDeclaredConstructors();
+            for (Constructor constructor : constructors) {
+                constructor.setAccessible(true);
+                reflectionInstance = (Singleton) constructor.newInstance();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("Singleton Instance hashCode: " + singletonInstance.hashCode());
+        System.out.println("Reflection Instance hashCode: " + reflectionInstance.hashCode());
+    }
+}
